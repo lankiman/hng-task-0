ac934a1f7d6c705ce01953e2646b4c4811601c17
Lankiman <marvellousgboun@gmail.com>
2025-01-30 22:07:19 +0100

    implmented endpoint to return specified data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
+using System.Globalization;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors();
+
+builder.Services.Configure<JsonOptions>(options =>
+    {
+options.SerializerOptions.PropertyNamingPolicy=JsonNamingPolicy.SnakeCaseLower;
+    }
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(options => {
+    options.AllowAnyOrigin()
+        .WithMethods("GET")
+        .AllowAnyHeader();
+});
+
+
+app.MapGet("/", () =>
+{
+    string isoDateTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss")+"Z";
+    return Results.Json(new
+    {
+        email = "marvellousboun@gmail.com",
+        currentDatetime = isoDateTime,
+        githubUrl="<https://github.com/lankiman/hng-task-0>"
+        
+
+    });
+});
 
 app.Run();
